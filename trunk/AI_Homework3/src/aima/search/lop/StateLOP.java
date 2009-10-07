f6ae65ba5a499394d7e9654fc3d9b137f1c1dbc9
antosha.suharev <antosha.suharev@e9796568-b0a7-11de-9ad3-fd5786690477>
2009-10-07 13:45:09 +0000

    generator gotov.

--- trunk/AI_Homework3/src/aima/search/lop/StateLOP.java
+++ trunk/AI_Homework3/src/aima/search/lop/StateLOP.java
@@ -1,13 +1,13 @@
-/**
- * 
- */
 package aima.search.lop;
+
+import java.util.Random;
 
 public class StateLOP {
 	
 	public int [][] A;
 	public int nLen;
 	public int minDist;
+	public int nParticipants;
 	
 	public int index;
 	public int cost;
@@ -27,9 +27,33 @@ public class StateLOP {
 
 	public void generateMatrix()
 	{
-		A = new int [] [] {{0, 16, 15, 12, 5}, {14, 0, 4, 16, 1}, {15, 26, 0, 19, 2}, {18, 14, 11, 0, 12}, {25, 29, 28, 18, 0}};
+		//A = new int [] [] {{0, 16, 15, 12, 5}, {14, 0, 4, 16, 1}, {15, 26, 0, 19, 2}, {18, 14, 11, 0, 12}, {25, 29, 28, 18, 0}};
 		nLen = 5;
 		minDist = 10;
+		nParticipants = 30;
+				
+		Random gen = new Random();
+		
+		A = new int[nLen][nLen];
+		for(int i = 0; i < nLen; i++){
+			for(int j = 0; j <= i; j++){
+				if(i == j){
+					A[i][j] = 0;
+				}else{
+					A[i][j] = gen.nextInt(nParticipants + 1);  
+					A[j][i] = nParticipants - A[i][j]; 
+						
+				}
+			}
+		}	
+		System.out.println("The Matrix is: ");
+		for(int k = 0; k < nLen; k++){
+			for(int l = 0; l < nLen; l++){
+				System.out.print(A[k][l] + "  ");
+			}
+			System.out.print("\n");
+		}
+		
 		
 	}
 	
